c67369a4061b33b1ab5962647c22d9bc47736527
Anup <anup-pradhan@hotmail.com>
2014-05-08 10:32:08 -0700

    made changes to site

---
+++ index_example.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+ 
+
+$("#anup").on("click", function() {
+
+$('#anup').css("background-color","#F28500");
+$('body').css("background-image", 'url(BG-Basketball_Court1.jpg)');
+     $('.skils').css('font-size' ,'20px');
+})
+ 
+ $("#blog").on("click", function() {
+
+$('#blog').css("background-color","#F28500");
+$('body').css("background-image", 'url(BG-Basketball_Court1.jpg)');
+     $('.skils').css('font-size' ,'20px');
+})
+ 
+ $("#skills").on("click", function() {
+
+$('#skills').css("background-color","#F28500");
+$('body').css("background-image", 'url(BG-Basketball_Court1.jpg)');
+     $('.skils').css('font-size' ,'20px');
+})
+ 
+ $("#interests").on("click", function() {
+
+$('#interests').css("background-color","#F28500");
+$('body').css("background-image", 'url(BG-Basketball_Court1.jpg)');
+     $('.skils').css('font-size' ,'20px');
+})
+ 
+
+
+
+    $('.small1').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src','google2.png')
+  })
+
+  $('.small1').on('mouseout', function(e){
+     e.preventDefault()
+    $(this).attr('src','google.jpg')
+  })
+
+ 
+ 
+ 
+})  
